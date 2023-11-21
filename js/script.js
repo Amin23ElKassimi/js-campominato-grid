ad7adf620addb343daf6826d888d0943e32cb3b1
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-21 20:04:09 +0100

    play

--- js/script.js
+++ js/script.js
@@ -7,10 +7,15 @@ function generateSquare() {
 }
 
 // recupero il parent all'interno del quale voglio inserire i miei elementi
-const mainContentEl = document.querySelector('main .main-content');
+const mainContentEl = document.getElementById('play');
 
+// Play button
+const play = document.querySelector('button');
+
+play.addEventListener('click', function(){
 // # per 100 iterazioni...
 for (let i = 1 ; i <= 100 ; i++){
+
     // # creo un nuovo elemento quadrato, una nuova cella nel mio quadrato
     const currentSquare = generateSquare();
 
@@ -24,12 +29,12 @@ for (let i = 1 ; i <= 100 ; i++){
     currentSquare.addEventListener('click', function(){
         // % metto o tolgo la classe css clicked allo stesso elemento
         currentSquare.classList.toggle('clicked');
-        
+        // Allert del numero selezionato
+        alert(squareContent);
     });
-
 
     // & aggiungo la cella completa all'elemento a cui voglio aggiungerla nel DOM.
     mainContentEl.appendChild(currentSquare);
 }
-
+});
 
