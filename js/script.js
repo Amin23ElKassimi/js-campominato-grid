a6d4d0843f1a0aaf0590e15f560d73171c870fe3
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-21 20:25:51 +0100

    Bonus 1 - 2 - 3

--- js/script.js
+++ js/script.js
@@ -5,36 +5,99 @@ function generateSquare() {
         squareElement.classList.add('square');
         return squareElement;
 }
+// Funzione 03       funzione che genera un nuovo elemento quadrato
+function generateSquare2() {
+    const squareElement = document.createElement('div');
+    squareElement.classList.add('square2');
+    return squareElement;
+}
+// Funzione 03       funzione che genera un nuovo elemento quadrato
+function generateSquare3() {
+    const squareElement = document.createElement('div');
+    squareElement.classList.add('square3');
+    return squareElement;
+}
 
 // recupero il parent all'interno del quale voglio inserire i miei elementi
-const mainContentEl = document.getElementById('play');
+const mainContentEl = document.querySelector('main .main-content');
 
 // Play button
-const play = document.querySelector('button');
+const play = document.getElementById('play');
+
+// Difficolta
+const difficulty = 3;
 
 play.addEventListener('click', function(){
-// # per 100 iterazioni...
-for (let i = 1 ; i <= 100 ; i++){
 
-    // # creo un nuovo elemento quadrato, una nuova cella nel mio quadrato
-    const currentSquare = generateSquare();
+    if (difficulty === 1) {
+        // # per 100 iterazioni...
+        for (let i = 1 ; i <= 100 ; i++){
+            const currentSquare = generateSquare();
 
-    // # inizializzo il suo contenuto per poterlo utilizzare anche piu' avanti
-    const squareContent = i;
+            // # inizializzo il suo contenuto per poterlo utilizzare anche piu' avanti
+            const squareContent = i;
+        
+            // # aggiungo il contenuto all'elemento che voglio popolare
+            currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+        
+            // % quando clicco su una di queste celle
+            currentSquare.addEventListener('click', function(){
+                // % metto o tolgo la classe css clicked allo stesso elemento
+                currentSquare.classList.toggle('clicked');
+                // Allert del numero selezionato
+                alert(squareContent);
+            });
+        
+            // & aggiungo la cella completa all'elemento a cui voglio aggiungerla nel DOM.
+            mainContentEl.appendChild(currentSquare);
+        }
+    }
+    if (difficulty === 2) {
+        // # per 100 iterazioni...
+        for (let i = 1 ; i <= 81 ; i++){
+        // # creo un nuovo elemento quadrato, una nuova cella nel mio quadrato
+            const currentSquare = generateSquare2();
 
-    // # aggiungo il contenuto all'elemento che voglio popolare
-    currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+            // # inizializzo il suo contenuto per poterlo utilizzare anche piu' avanti
+            const squareContent = i;
+        
+            // # aggiungo il contenuto all'elemento che voglio popolare
+            currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+        
+            // % quando clicco su una di queste celle
+            currentSquare.addEventListener('click', function(){
+                // % metto o tolgo la classe css clicked allo stesso elemento
+                currentSquare.classList.toggle('clicked');
+                // Allert del numero selezionato
+                alert(squareContent);
+            });
+        
+            // & aggiungo la cella completa all'elemento a cui voglio aggiungerla nel DOM.
+            mainContentEl.appendChild(currentSquare);
+        }
+    }
+    if (difficulty === 3) {
+        // # per 100 iterazioni...
+        for (let i = 1 ; i <= 49 ; i++){
+        // # creo un nuovo elemento quadrato, una nuova cella nel mio quadrato
+            const currentSquare = generateSquare3();
 
-    // % quando clicco su una di queste celle
-    currentSquare.addEventListener('click', function(){
-        // % metto o tolgo la classe css clicked allo stesso elemento
-        currentSquare.classList.toggle('clicked');
-        // Allert del numero selezionato
-        alert(squareContent);
-    });
-
-    // & aggiungo la cella completa all'elemento a cui voglio aggiungerla nel DOM.
-    mainContentEl.appendChild(currentSquare);
-}
+            // # inizializzo il suo contenuto per poterlo utilizzare anche piu' avanti
+            const squareContent = i;
+        
+            // # aggiungo il contenuto all'elemento che voglio popolare
+            currentSquare.innerHTML += `<span> ${squareContent} </span>`;
+        
+            // % quando clicco su una di queste celle
+            currentSquare.addEventListener('click', function(){
+                // % metto o tolgo la classe css clicked allo stesso elemento
+                currentSquare.classList.toggle('clicked');
+                // Allert del numero selezionato
+                alert(squareContent);
+            });
+        
+            // & aggiungo la cella completa all'elemento a cui voglio aggiungerla nel DOM.
+            mainContentEl.appendChild(currentSquare);
+        }
+    }
 });
-
